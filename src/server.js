de7b4755613d2de5574461595c7080b4c64e7fcd
Kim <cid.kim@gmail.com>
2023-06-18 18:41:51 -0600

    added a post route to app

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/:id', (request, response) => {
   const { id } = request.params;
@@ -13,6 +14,14 @@ app.get('/', (request, response) => {
   response.send(`Eu gosto de comer ${num} ${prato} no almoço`);
 });
 
+app.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  response.send({ name, email });
+});
+
 const PORT = 3333;
 
-app.listen(PORT, () => console.log(`Xis salada is running on Port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Xis salada is running on Port ${PORT} e isso não importa`)
+);

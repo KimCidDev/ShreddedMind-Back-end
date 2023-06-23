a312ea8ff267bab529718464e2e9f1903ce6e9ff
Kim <cid.kim@gmail.com>
2023-06-23 12:45:39 -0600

    'test'

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -2,7 +2,7 @@ class UsersController {
   create(request, response) {
     const { name, email, password } = request.body;
 
-    response.status(201).json({ name, email, password });
+    response.status(204).json({ name, email, password });
   }
 }
 

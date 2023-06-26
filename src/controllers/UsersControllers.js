f610ba38c21b72bff313f593eb727b578eee6136
Kim <cid.kim@gmail.com>
2023-06-26 06:21:31 -0600

    express async errors installed and rules for standart error structure are set

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -4,6 +4,12 @@ class UsersController {
   create(request, response) {
     const { name, email, password } = request.body;
 
+    if (!name) {
+      throw new AppError(
+        'O nome é Xis Salada ou outro que você escolher, mas pelo menos Xis Salada é obrigatório'
+      );
+    }
+
     response.status(201).json({ name, email, password });
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+require('express-async-errors');
+
+const AppError = require('./utils/AppError');
+
 const express = require('express');
 const app = express();
 
@@ -5,6 +9,22 @@ const routes = require('./routes');
 
 app.use(express.json());
 app.use(routes);
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: AppError.message
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'internal server error'
+  });
+});
 
 const PORT = 3333;
 

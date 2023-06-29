101abfd641be26020034b8b43d11c572427f4eae
Kim <cid.kim@gmail.com>
2023-06-29 15:00:03 -0600

    migrations config started

---
+++ src/database/knex/migrations/20230629203108_createNotes.js
@@ -0,0 +1,13 @@
+
+exports.up = knex => knex.schema.createTable("notes", table => {
+  table.increments("id");
+  table.text()
+  table.
+  table.
+  table.
+  table.
+
+});
+
+exports.down =  knex => knex.schema.dropTable("notes")
+;

c8d34f7654fc448903c7f479f7a5d53868bb7392
Kim <cid.kim@gmail.com>
2023-06-30 05:59:34 -0600

    created notes table and links table

--- src/database/knex/migrations/20230629203108_createNotes.js
+++ src/database/knex/migrations/20230629203108_createNotes.js
@@ -1,13 +1,12 @@
+exports.up = knex =>
+  knex.schema.createTable('notes', table => {
+    table.increments('id');
+    table.text('title');
+    table.text('description');
+    table.integer('user_id').references('id').inTable('users');
 
-exports.up = knex => knex.schema.createTable("notes", table => {
-  table.increments("id");
-  table.text()
-  table.
-  table.
-  table.
-  table.
+    table.timestamp('created_at').default(knex.fn.now());
+    table.timestamp('updated_at').default(knex.fn.now());
+  });
 
-});
-
-exports.down =  knex => knex.schema.dropTable("notes")
-;
+exports.down = knex => knex.schema.dropTable('notes');

97498be421151ecbe35a392667eba44d54f40911
Kim <cid.kim@gmail.com>
2023-07-12 06:02:13 -0600

    Sessions verifying email and password

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,24 @@
+const knex = require('../database/knex');
+const AppError = require('../utils/AppError');
+const { compare } = require('bcryptjs');
+
 class SessionsController {
   async create(request, response) {
-    
+    const { email, password } = request.body;
+
+    const user = await knex('users').where({ email }).first();
+
+    if (!user) {
+      throw new AppError('Tu tá viajando. Não tem esse aí não', 404);
+    }
+
+    const passwordCheck = await compare(password, user.password);
+
+    if (!passwordCheck) {
+      throw new AppError('Tu tá viajando. Essa senha tá incorreta', 404);
+    }
+
+    return response.json({ user });
   }
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,10 @@ const UserRoutes = require('./users.routes');
 const NotesRoutes = require('./notes.routes');
 const TagsRoutes = require('./tags.routes');
 const LinksRoutes = require('./links.routes');
+const SessionsRoutes = require('./sessions.routes');
 
 routes.use('/users', UserRoutes);
+routes.use('/sessions', SessionsRoutes);
 routes.use('/notes', NotesRoutes);
 routes.use('/tags', TagsRoutes);
 //routes.use('/links', LinksRoutes);

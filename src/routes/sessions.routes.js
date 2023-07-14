4fab716bdb7c22580653d81cd22c9676e7ea1ada
Kim <cid.kim@gmail.com>
2023-07-14 14:29:16 -0600

    middleware working before userController.update

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 
 const SessionsController = require('../controllers/SessionsController');
 const sessionsController = new SessionsController();
+const ensureAuthenticated = require('../middleware/ensureAuthenticated');
 
 const sessionRoutes = Router();
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,10 +2,11 @@ const { Router } = require('express');
 
 const UsersController = require('../controllers/UsersControllers');
 const usersController = new UsersController();
+const ensureAuthenticated = require('../middleware/ensureAuthenticated');
 
 const UserRoutes = Router();
 
 UserRoutes.post('/', usersController.create);
-UserRoutes.put('/:id', usersController.update);
+UserRoutes.put('/', ensureAuthenticated, usersController.update);
 
 module.exports = UserRoutes;
